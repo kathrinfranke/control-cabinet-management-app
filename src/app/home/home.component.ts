8bc928691707543e51eeefb42c4e900e7c9c6007
Kathrin Franke <kathrinfranke@yahoo.de>
2018-11-04 21:15:56 +0100

    feat: welcome-page-contents

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  cabinet_counter: any;
+  device_counter: any;
 
-  constructor() { }
+  constructor(private data: DataService) {
+    this.cabinet_counter = Object.keys(this.data.getLocalStorageData('cabinets')).length;
+    this.device_counter = Object.keys(this.data.getLocalStorageData('devices')).length;
+  }
 
   ngOnInit() {
   }
