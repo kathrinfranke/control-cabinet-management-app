9a5039cdb05217f1ef6c0074db35e734ad074c41
Kathrin Franke <kathrinfranke@yahoo.de>
2018-11-04 23:08:31 +0100

    fix counters at HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,8 +11,16 @@ export class HomeComponent {
   device_counter: any;
 
   constructor(private data: DataService) {
-    this.cabinet_counter = Object.keys(this.data.getLocalStorageData('cabinets')).length;
-    this.device_counter = Object.keys(this.data.getLocalStorageData('devices')).length;
+    if (this.data.getLocalStorageData('cabinets') === null) {
+      this.cabinet_counter = 'keine';
+    } else {
+      this.cabinet_counter = Object.keys(this.data.getLocalStorageData('cabinets')).length;
+    }
+    if (this.data.getLocalStorageData('devices') === null) {
+      this.device_counter = 'keine';
+    } else {
+      this.device_counter = Object.keys(this.data.getLocalStorageData('devices')).length;
+    }
   }
 
   ngOnInit() {
